bf92b296ff84b6f2abc185fa20f99c89cc886368
Sae Rose <sbarcelocompte@gmail.com>
2018-09-22 20:29:02 +0200

    chore: result styling

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import StarRatings from 'react-star-ratings';
 import '../components_sass/Result.sass';
 
-import { onDetectedReducer } from '../redux/actions';
 import { connect } from 'react-redux';
 
 import tombstone from '../assets/tombstone.svg';
 
 class Result extends Component {
+  changeRating( newRating, name ) {
+    this.setState({
+      rating: newRating
+    });
+  }
 
   render() {
     const result = {"kind":"books#volume","id":"F9ybAgAAQBAJ","etag":"VTVItL6yr6Q","selfLink":"https://www.googleapis.com/books/v1/volumes/F9ybAgAAQBAJ","volumeInfo":{"title":"JavaScript","subtitle":"The Good Parts","authors":["Douglas Crockford"],"publisher":"\"O'Reilly Media, Inc.\"","publishedDate":"2008-05-08","description":"Describes the reliable features of JavaScript, covering such topics as syntax, objects, functions, arrays, regular expressions, inheritance, and methods.","industryIdentifiers":[{"type":"ISBN_13","identifier":"9780596517748"},{"type":"ISBN_10","identifier":"0596517742"}],"readingModes":{"text":false,"image":false},"pageCount":153,"printType":"BOOK","categories":["Computers"],"averageRating":4,"ratingsCount":8,"maturityRating":"NOT_MATURE","allowAnonLogging":false,"contentVersion":"preview-1.0.0","imageLinks":{"smallThumbnail":"http://books.google.com/books/content?id=F9ybAgAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api","thumbnail":"http://books.google.com/books/content?id=F9ybAgAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"},"language":"en","previewLink":"http://books.google.es/books?id=F9ybAgAAQBAJ&dq=isbn:9780596517748&hl=&cd=1&source=gbs_api","infoLink":"http://books.google.es/books?id=F9ybAgAAQBAJ&dq=isbn:9780596517748&hl=&source=gbs_api","canonicalVolumeLink":"https://books.google.com/books/about/JavaScript.html?hl=&id=F9ybAgAAQBAJ"},"saleInfo":{"country":"ES","saleability":"NOT_FOR_SALE","isEbook":false},"accessInfo":{"country":"ES","viewability":"NO_PAGES","embeddable":false,"publicDomain":false,"textToSpeechPermission":"ALLOWED","epub":{"isAvailable":false},"pdf":{"isAvailable":false},"webReaderLink":"http://play.google.com/books/reader?id=F9ybAgAAQBAJ&hl=&printsec=frontcover&source=gbs_api","accessViewStatus":"NONE","quoteSharingAllowed":false},"searchInfo":{"textSnippet":"Describes the reliable features of JavaScript, covering such topics as syntax, objects, functions, arrays, regular expressions, inheritance, and methods."}}//this.props.results;
@@ -23,13 +28,28 @@ class Result extends Component {
               <div className='Result_topinfo_wrapper_categories'>{result.volumeInfo.categories}</div>
               <div className='Result_topinfo_wrapper_title'>{result.volumeInfo.title}</div>
               <div className='Result_topinfo_wrapper_authors'>by {result.volumeInfo.authors}</div>
-              <div className='Result_topinfo_wrapper_rating'>Rating: {result.volumeInfo.averageRating}</div>
+              <div className='Result_topinfo_wrapper_rating'>
+                <StarRatings
+                  rating={result.volumeInfo.averageRating}
+                  starRatedColor="#F8E473"
+                  changeRating={this.changeRating}
+                  numberOfStars={5}
+                  starDimension={22}
+                  starSpacing={2}
+                  name='rating'
+                />
+              </div>
             </div>
           </div>
         </div>
 
         <div className='Result_bottominfo_outerdiv'>
-          <div className='Result_bottominfo_sinopsis'>SYNOPSIS</div>
+          <div className='Result_bottominfo_sinopsis'>Synopsis</div>
+          <div className='Result_TextSnippet'>" {result.searchInfo.textSnippet} "</div>
+        </div>
+
+        <div className='Result_bottominfo_outerdiv'>
+          <div className='Result_bottominfo_sinopsis'>Reviews</div>
           <div className='Result_TextSnippet'>" {result.searchInfo.textSnippet} "</div>
         </div>
       </div>
