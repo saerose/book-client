4c91a126f0e1641d4347745e1d2d6abd6a574f5d
Sae Rose <sbarcelocompte@gmail.com>
2018-09-23 21:07:17 +0200

    feat: login backend working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { logIn } from '../redux/actions';
+
+import GoogleLogin from 'react-google-login';
+
 import '../components_sass/Menu.sass';
 import '../components_sass/Home.sass';
 
@@ -7,18 +12,51 @@ import openBook from '../assets/open-book.svg';
 
 
 class Home extends Component {
-  render() {
+  loginGoogle = ({ profileObj }) => {
+    fetch('http://localhost:3001/login/google', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(profileObj)
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.props.logIn(res)
+      })
+      .catch(err => console.error(err))
+  }
 
-    return(
+  render() {
+    return (
       <div className='Home_wrapper'>
         <div className='Home_wrapper_title'>Ello Bello</div>
         <div className='Home_wrapper_text'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
           </div>
         <img className='Home_wrapper_img' alt='OpenBook' src={openBook} />
+        {
+          !this.props.user.id && (
+            <GoogleLogin
+              clientId={process.env.GOOGLE_API_KEY}
+              buttonText="Login"
+              onSuccess={this.loginGoogle}
+              onFailure={console.error}
+            />
+          )
+        }
+
       </div>
     )
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  user: state.userReducer.user
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  logIn: (user) => dispatch(logIn(user))
+})
+
+export default connect (mapStateToProps, mapDispatchToProps)(Home);
