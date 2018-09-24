7b27b4bd3823359f1d93974a0d7f2cf89959651b
Sae Rose <sbarcelocompte@gmail.com>
2018-09-24 12:01:07 +0200

    feat: sign in with google

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { logIn } from '../redux/actions';
-
 import GoogleLogin from 'react-google-login';
 
 import '../components_sass/Menu.sass';
 import '../components_sass/Home.sass';
 
 import openBook from '../assets/open-book.svg';
-// const noteBookImg = require('../assets/diary.svg');
+import pileBooks from '../assets/pile-books.svg';
 
 
 class Home extends Component {
@@ -28,26 +27,36 @@ class Home extends Component {
   }
 
   render() {
-    return (
-      <div className='Home_wrapper'>
-        <div className='Home_wrapper_title'>Ello Bello</div>
-        <div className='Home_wrapper_text'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+    if (!this.props.user.id) {
+      return (
+        <div className='Home_signup_wrapper'>
+          <div className='Home_signup_title'>minerva</div>
+          <div className='Home_signup_text'>
+            Find, get and share books with your friends and millions of readers.
           </div>
-        <img className='Home_wrapper_img' alt='OpenBook' src={openBook} />
-        {
-          !this.props.user.id && (
+          <img className='Home_wrapper_signup_img' alt='Pile of books' src={pileBooks} />
+
             <GoogleLogin
-              clientId={process.env.GOOGLE_API_KEY}
-              buttonText="Login"
+              className ='Home_signup_googlebutton'
+              clientId='673352715028-kbn3igi1c3sg2apmbf1vos7stdv8n406.apps.googleusercontent.com'
+              buttonText="Login with Google"
               onSuccess={this.loginGoogle}
               onFailure={console.error}
             />
-          )
-        }
-
-      </div>
-    )
+        </div>
+      )
+    } else {
+      return (
+        <div className='Home_wrapper'>
+        <div className='Home_wrapper_title'>Welcome!</div>
+        <div className='Home_wrapper_text'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
+        </div>
+        <img className='Home_wrapper_img' alt='OpenBook' src={openBook} />
+        </div>
+      )
+    }
   }
 }
 
@@ -60,3 +69,29 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect (mapStateToProps, mapDispatchToProps)(Home);
+
+/*
+return (
+      <div className='Home_wrapper'>
+        <div className='Home_wrapper_title'>Ello Bello</div>
+        <div className='Home_wrapper_text'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+          </div>
+        <img className='Home_wrapper_img' alt='OpenBook' src={openBook} />
+        {
+          !this.props.user.id && (
+          <div>
+          <div>This is the sign up title</div>
+            <GoogleLogin
+              clientId={process.env.GOOGLE_API_KEY}
+              buttonText="Login"
+              onSuccess={this.loginGoogle}
+              onFailure={console.error}
+            />
+          </div>
+          )
+        }
+
+      </div>
+    )
+*/
