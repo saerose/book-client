937b988a03d03aae8aaff5e2becd7da15e29a347
Sae Rose <sbarcelocompte@gmail.com>
2018-09-24 19:10:10 +0200

    feat: base done

--- src/components/SearchResultItem.js
+++ src/components/SearchResultItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import StarRatings from 'react-star-ratings';
-import '../components_sass/Result.sass';
+import '../components_sass/SearchBarResultItem.sass';
 
 
 class SearchResultItem extends Component {
@@ -8,8 +8,26 @@ class SearchResultItem extends Component {
   render() {
     console.log(this.props)
     return (
-      <div>
-        <div>{this.props.title}</div>
+      <div className='SearchResult_wrapper'>
+        <div className='SearchResult_img_wrapper'>
+          <img className='SearchResult_img' alt='Book cover' src={this.props.imageLinks.thumbnail} />
+        </div>
+
+        <div className='SearchResult_info_wrapper'>
+          <div className='SearchResult_title'>{this.props.title}</div>
+          <div className='SearchResult_subtitle'>{this.props.subtitle}</div>
+          <div className='SearchResult_author'> by {this.props.authors}</div>
+
+          <StarRatings
+            rating={this.props.averageRating}
+            starRatedColor="#F8E473"
+            changeRating={this.changeRating}
+            numberOfStars={5}
+            starDimension={20}
+            starSpacing={2}
+            name='rating'
+          />
+        </div>
       </div>
     )
   }
