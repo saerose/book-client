bc25688cc70db0b929af8ae40a40cb5f8d183f4a
saerose <sbarcelocompte@gmail.com>
2018-09-25 13:50:25 +0200

    feat: added library component

---
+++ src/components/LibraryDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../components_sass/LibraryDashboard.sass'
+
+class LibraryDashboard extends Component {
+
+  render() {
+    return (
+      <div className='LibraryDashboard_wrapper'>I'm the library dashboard.
+        This section is still not working.</div>
+    );
+  }
+}
+
+export default LibraryDashboard;
