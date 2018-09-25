856947afdeb9539c1666c849e0012af0bc80ceae
saerose <sbarcelocompte@gmail.com>
2018-09-25 17:27:25 +0200

    chore: added info to render something

--- src/components/LibraryDashboard.js
+++ src/components/LibraryDashboard.js
@@ -5,8 +5,17 @@ class LibraryDashboard extends Component {
 
   render() {
     return (
-      <div className='LibraryDashboard_wrapper'>I'm the library dashboard.
-        This section is still not working.</div>
+      <div>
+        <div className='LibraryDashboard_bookshelf'>
+          <div className='LibraryDashboard_books'>000 <p>Books</p></div>
+          <div className='LibraryDashboard_collections'>000 <p>Series</p></div>
+          <div className='LibraryDashboard_toread'>000 <p>To Read</p></div>
+        </div>
+
+        <div className='LibraryDashboard_info'>
+        Sorry, this component is still under construction.
+        </div>
+      </div>
     );
   }
 }
