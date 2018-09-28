eac88fe8bc286c42f175957fa0d8f02c49771d4a
Peter Schindler <petertasschindler@gmail.com>
2018-09-28 13:41:41 +0200

    signup ui

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { logIn } from '../../redux/actions';
 import GoogleLogin from 'react-google-login';
+import { Link } from 'react-router-dom';
+
 
 import '../../components_sass/Menu.sass';
-import '../../components_sass/Home.sass';
+import '../../components_sass/Signup.sass';
 
 import openBook from '../../assets/open-book.svg';
 import pileBooks from '../../assets/pile-books.svg';
@@ -29,22 +31,72 @@ class Signup extends Component {
 
   render() {
       return (
-        <div className='Home_signup_wrapper'>
-          <div className='Home_signup_title'>minerva</div>
-          <div className='Home_signup_text'>
-            Signup
-          </div>
-          <img className='Home_wrapper_signup_img' alt='Pile of books' src={pileBooks} />
+        <div className='Signup_signup_wrapper'>
 
-            <div className='Home_signup_wrapper_googlebutton'>
+
+          <div className='Signup_signup_title'>minerva</div>
+
+
+          <img className='Signup_wrapper_signup_img' alt='Pile of books' src={pileBooks} />
+
+
+          <div className='Signup_socialmedia_signup_buttons'>
+            <div className='Signup_signup_wrapper_googlebutton'>
               <GoogleLogin
-                className ='Home_signup_googlebutton'
+                className ='Signup_signup_googlebutton'
                 clientId='673352715028-kbn3igi1c3sg2apmbf1vos7stdv8n406.apps.googleusercontent.com'
                 buttonText="Login with Google"
                 onSuccess={this.loginGoogle}
                 onFailure={console.error}
               />
             </div>
+            <div className='Signup_signup_wrapper_facebookbutton'>
+              <GoogleLogin
+                className ='Signup_signup_facebookbutton'
+                clientId='673352715028-kbn3igi1c3sg2apmbf1vos7stdv8n406.apps.googleusercontent.com'
+                buttonText="Login with Facebook"
+                onSuccess={this.loginGoogle}
+                onFailure={console.error}
+              />
+            </div>
+          </div>
+
+
+          <div className='Signup_signup_text'>
+            or
+          </div>
+
+
+          <div className='Signup_signup_form'>
+            <div className='Signup_signup_inputs'>
+              <div>
+                <input className='Signup_signup_input' type="text" placeholder="Full Name"/>
+                <input className='Signup_signup_input' type="text" placeholder="Email Address"/>
+                <input className='Signup_signup_input' type="text" placeholder="Password"/>
+              </div>
+            </div>
+            <button className='Signup_signup_button'>
+              <strong>Sign Up!</strong>
+            </button>
+          </div>
+
+
+          <div className='Signup_to_login'>
+            <div>
+              <div className='Signup_signup_text'>
+                Already have an account?
+              </div>
+            </div>
+            <div>
+              <Link to="/login" >
+                <div className='Signup_signup_text Signup_to_login_button'>
+                  <u>Log In!</u>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+
         </div>
       )
   }
